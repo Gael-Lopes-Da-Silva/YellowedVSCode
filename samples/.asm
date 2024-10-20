ca835cafdb0680b34b6c9166b6d6be53dce91c03
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-10-20 23:26:20 +0200

    minor update in config

--- samples/.asm
+++ samples/.asm
@@ -1,14 +1,22 @@
-          global    _start
+; use the build-linux.sh script to assemble and link this
+        bits 64
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+section .text
 
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+        global _start
+_start:
+        mov     rax, 1 ; write
+        mov     rdi, 1 ; stdout
+        mov     rsi, msg
+        mov     rdx, msg.len
+        syscall
+
+        mov     rax, 60 ; exit
+        mov     rdi, 0
+        syscall
+
+
+        section .data
+
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
